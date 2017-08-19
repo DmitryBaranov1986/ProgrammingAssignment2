830a85e3ee35b1e28b22c7d73b673878a5170e3b
DmitryBaranov1986 <satensera@gmail.com>
2017-08-20 01:02:33 +0300

    Solved the matrix caching task

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,46 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    slv <- NULL
+    
+    set <- function(m) 
+    {
+        mtx <<- m
+        slv <<- NULL
+    }
+    
+    get <- function()
+    {
+        mtx
+    }
+    
+    setSlv <- function(i)
+    {
+        slv <<- i
+    }
+    
+    getSlv <- function()
+    {
+        slv
+    }
+    
+    list(set = set, get = get, setSlv = setSlv, getSlv = getSlv)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    slv <- x$getSlv()
+    if(!is.null(slv))
+    {
+        message("Getting cached matrix")
+        return (slv)
+    }
+    message("Evaluating inverse matrix")
+    data <- x$get()
+    slv <- solve(data)
+    x$setSlv(slv)
+    slv
 }
